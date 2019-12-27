8f68ab07e82fed310c4cae90ab3f9c60dce591a9
choisungyu <maroon92@naver.com>
2019-12-27 15:06:05 +0900

    v-bind & vue style & webpack-dev-server적용

---
+++ response-check/main.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+
+import ResponseCheck from './response-check';
+
+new Vue(ResponseCheck).$mount('#root');
---
+++ response-check/response-check.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div id="screen" :class="state" @click="onClickState">{{message}}</div>
+    <div>
+      <div>평균시간 : {{}}</div>
+      <button @click="onReset">리셋버튼</button>
+    </div>
+  </div>
+</template>
+
+
+<script>
+// import ~from 과 같다
+export default {
+  data() {
+    return {
+      reset:'',
+      state: 'waiting',
+      message:'클릭해서 시작',
+    
+
+
+      
+    };
+  },
+  methods: {
+    onReset() {
+
+    },
+
+    onClickState() {
+      if (this.state === 'waiting') {
+        this.state = 'ready';
+
+      } else if (this.state === 'ready') {
+        this.state = 'now';
+        
+      } else if (this.state === 'now') {
+        this.state = 'waiting';
+        
+      }
+    },
+    
+  }
+};
+</script>
+
+<style scoped>
+#screen {
+  width: 300px;
+  height: 200px;
+  text-align: center;
+  user-select: none;
+}
+#screen.waiting {
+  background-color: aqua;
+}
+#screen.ready {
+  background-color: red;
+  color:white;
+}
+#screen.now {
+  background-color: greenyellow;
+}
+</style>
