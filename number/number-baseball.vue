4a6a25fb8e0c309bc14864a7d0003ba572c7849e
choisungyu <maroon92@naver.com>
2019-12-27 13:35:42 +0900

    숫자야구 완성

--- number/number-baseball.vue
+++ number/number-baseball.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <h1>{{result}}</h1>
-    <form v-on:submit="onSubmitForm">
+    <form @submit.prevent="onSubmitForm">
       <input ref="answer" minlength="4" maxlength="4" v-model="value" type="text" placeholder="숫자" />
       <button>입력</button>
     </form>
     <div>시도 : {{tries.length}}</div>
     <ul>
-      <li v-for="t in tries" v-bind:key="t">
+      <li v-for="t in tries" :key="t">
         <div>{{t.try}}</div>
         <div>{{t.result}}</div>
       </li>
@@ -16,9 +16,21 @@
 </template>
 
 <script>
+const getNumbers = () => {
+  const candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const array = [];
+  for (let i = 0; i < 4; i += 1) {
+    const chosen = candidates.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+    array.push(chosen);
+  }
+  return array;
+};
+
+// import ~from 과 같다
 export default {
   data() {
     return {
+      answer: getNumbers(),
       tries: [],
       value: "",
       result: ""
@@ -26,13 +38,47 @@ export default {
   },
   methods: {
     onSubmitForm(e) {
-      e.preventDefault();
-      this.tries.push({
-        try: this.value,
-        result: "홈런"
-      });
-      this.value = "";
-      this.$refs.answer.focus();
+      // e.preventDefault();
+      if (this.value === this.answer.join("")) {
+        this.tries.push({
+          try: this.value,
+          result: "홈런"
+        });
+        this.result = "홈런";
+        alert("게임을 다시 시작");
+        this.answer = getNumbers();
+        this.tries = [];
+        this.value = "";
+        this.$refs.answer.focus();
+      } else {
+        if (this.tries.length >= 9) {
+          this.result = `10번 넘게 돌려서 실패! 답은 ${this.answer.join(
+            ","
+          )} 였습니다.`;
+          alert("게임을 다시 시작합니다.");
+          this.answer = getNumbers();
+          this.tries = [];
+          this.value = "";
+          this.$refs.answer.focus();
+        }
+
+        let strike = 0;
+        let ball = 0;
+        const answerArray = this.value.split("").map(v => parseInt(v));
+        for (let i = 0; i < 4; i += 1) {
+          if (answerArray[i] === this.answer[i]) {
+            strike++;
+          } else if (this.answer.includes(answerArray[i])) {
+            ball++;
+          }
+        }
+        this.tries.push({
+          try: this.value,
+          result: `${strike} 스트라이크 , ${ball} 볼 입니다.`
+        });
+        this.value = "";
+        this.$refs.answer.focus();
+      }
     }
   }
 };
