8ec45a9ee875c3c211c725ab854817308217d9d3
choisungyu <maroon92@naver.com>
2019-12-28 10:59:52 +0900

    가위바위보 완성

--- rock-sissors-paper/rock-sissors-paper.vue
+++ rock-sissors-paper/rock-sissors-paper.vue
@@ -11,63 +11,84 @@
   </div>
 </template>
 
-
 <script>
-// import ~from 과 같다
-const rspCords = {
-  rock: "0",
-  sissros: "-142px",
-  paper: "-284px"
+const rspCoords = {
+  바위: "0",
+  가위: "-142px",
+  보: "-284px"
 };
-
+const scores = {
+  가위: 1,
+  바위: 0,
+  보: -1
+};
+const computerChoice = imgCoord => {
+  return Object.entries(rspCoords).find(function(v) {
+    return v[1] === imgCoord;
+  })[0];
+};
 let interval = null;
 export default {
   data() {
     return {
-      imgCord: rspCords.sissros,
+      imgCoord: rspCoords.바위,
       result: "",
       score: 0
     };
   },
-
-  // 계산하는 로직, 곁따리들 => chaching 처리
   computed: {
     computedStyleObject() {
       return {
-        background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${this.imgCord} 0`
+        background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${this.imgCoord} 0`
       };
     }
   },
-
   methods: {
-    onClickButton(choice) {}
+    changeHand() {
+      interval = setInterval(() => {
+        if (this.imgCoord === rspCoords.바위) {
+          this.imgCoord = rspCoords.가위;
+        } else if (this.imgCoord === rspCoords.가위) {
+          this.imgCoord = rspCoords.보;
+        } else if (this.imgCoord === rspCoords.보) {
+          this.imgCoord = rspCoords.바위;
+        }
+      }, 100);
+    },
+    onClickButton(choice) {
+      clearInterval(interval);
+      const myScore = scores[choice];
+      const cpuScore = scores[computerChoice(this.imgCoord)];
+      const diff = myScore - cpuScore;
+      if (diff === 0) {
+        this.result = "비겼습니다.";
+      } else if ([-1, 2].includes(diff)) {
+        this.result = "이겼습니다.";
+        this.score += 1;
+      } else {
+        this.result = "졌습니다.";
+        this.score -= 1;
+      }
+      setTimeout(() => {
+        this.changeHand();
+      }, 3000);
+    }
   },
 
   beforeUpdate() {
-    console.log('beforeupdate')
+    console.log("beforeupdate");
   },
 
   updated() {
-    console.log('updated');
-    
+    console.log("updated");
   },
   mounted() {
-    interval = setInterval(() => {
-      if (this.imgCord === rspCords.rock) {
-        this.imgCord = rspCords.sissros;
-      } else if (this.imgCord === rspCords.sissros) {
-        this.imgCord = rspCords.paper;
-      } else if (this.imgCord === rspCords.paper) {
-        this.imgCord = rspCords.rock;
-      }
-    }, 100);
+    this.changeHand;
   },
 
   beforeDestroy() {
     clearInterval(interval);
-  },
-
-  
+  }
 };
 </script>
 
